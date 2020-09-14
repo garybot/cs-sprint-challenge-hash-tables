5afb8a81f929c5d9159bd82094e43b96074953a3
garybot <argot.pryer@gmail.com>
2020-09-13 18:13:14 -0700

    Complete ex1.py.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,11 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}
+    
+    for i in range(length):
+        cache[weights[i]] = i
 
+    for i in range(length):       
+        if cache.get(limit - weights[i]):
+            return [cache.get(limit - weights[i]), i]
+            
     return None
