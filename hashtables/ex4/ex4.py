27f93b23a567db5d4f7fb411e2ba21fb1fb69d18
garybot <argot.pryer@gmail.com>
2020-09-13 18:46:50 -0700

    Complete ex4.py

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,9 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    table = {}
+    for i in a:
+        table[i] = True
+    result = [i for i in a if i > 0 and table.get(i*-1)]
     return result
-
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
